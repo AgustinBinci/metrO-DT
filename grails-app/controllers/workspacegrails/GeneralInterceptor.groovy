5768a484026e3b192ffec09aef322cf836c2dc55
AgustinBinci <agustinbinci@gmail.com>
2016-03-29 22:00:42 -0300

    Implemento interceptores

--- grails-app/controllers/workspacegrails/GeneralInterceptor.groovy
+++ grails-app/controllers/workspacegrails/GeneralInterceptor.groovy
@@ -4,16 +4,32 @@ package workspacegrails
 class GeneralInterceptor {
 
     GeneralInterceptor() {
-	    matchAll().excludes(controller:'usuario', action:'login')
-	    matchAll().excludes(controller:'usuario', action:'create')
+	    match(controller: "fechaActual");
+	    match(controller: "opcionesDelJuego");
+	    match(controller: "club");
+	    match(controller: "jugador");
+	    match(controller: "equipoDeUnaFecha");
+	    match(controller: "usuario", action: "edit");
+	    match(controller: "usuario", action: "index");
+	    match(controller: "usuario", action: "show");
+	    match(controller: "usuario", action: "showEquipo");
+	    match(controller: "usuario", action: "cambiarContrasenia");
+	    match(controller: "usuario", action: "delete");
+	    match(controller: "usuario", action: "guardarUsuario");
+	    match(controller: "usuario", action: "logout");
     }
 
-    boolean before() {
+	protected Boolean validarSesion() {
         if(!session.user) {
-            redirect(controller: "usuario", action: "login");
-            return true;
-        } 
-	return true;
+		  flash.errorMessage = "Debes ingresar para acceder a esa pagina";
+            chain(controller: "usuario", action: "login");
+            return false;
+        }
+		return true;
+	}
+
+    boolean before() {
+		this.validarSesion();
      }
 
     boolean after() { true }
